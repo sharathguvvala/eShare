f5e90cdc6df47427ace82e67861aa607f0b21df4
sharathguvvala <guvvalasharath@gmail.com>
2021-08-04 00:44:02 +0530

    added home.ejs

---
+++ controllers/homeController.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res){
+    return res.render('home.ejs',{title:"eShare | Home"})
+}
--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express()
 const port = process.env.port || 8000
 const expressLayouts = require('express-ejs-layouts')
 
+app.use(express.static('./assets'))
+
 app.use(expressLayouts)
-app.use('layout extractStyles',true)
-app.use('layout extractScripts',true)
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
 
 app.set('view engine','ejs')
 app.set('views','./views')
+
+app.use("/", require('./routes/index')) 
 
 app.listen(port,function(err){
     if(err){
