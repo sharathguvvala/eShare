1a79b06af2f6ffbe4638bedcef5f13632ca6ace2
Sharath Kumar Reddy Guvvala <guvvalasharath.kumar2020@vitstudent.ac.in>
2021-08-20 15:39:44 +0530

    added user settings

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,25 +1,14 @@
 const User = require('../models/user')
 const multer = require('multer')
 const sharp = require('sharp')
+const bcrypt = require('bcrypt')
 
 module.exports.view = function(req,res){
-    User.findById(req.params.id,function(err,user){
-        if(err){
-            console.log('Error in finding user',err)
-            return
-        }
-        return res.render('Profile/userProfile',{title:`Profile |  ${user.username}`,profileUser:user})
-    })
+    return res.render('Profile/userProfile',{title:`Profile`})
 }
 
 module.exports.edit = function(req,res){
-    User.findById(req.params.id,function(err,user){
-        if(err){
-            console.log('Error in finding user',err)
-            return
-        }
-        return res.render('Profile/editProfile',{title:`Edit Profile | ${user.username}`,profileUser:user})
-    })
+    return res.render('Profile/editProfile',{title:`Edit Profile`})
 }
 
 module.exports.upload = multer({
@@ -48,5 +37,29 @@ module.exports.editprofile = async function(req,res){
         user.save()
         req.flash('info','Your Profile was successfully updated!!!')
         return res.redirect('/')
+    })
+}
+
+module.exports.settings = function(req,res){
+    return res.render('Profile/userSettings',{title:`Profile`})
+}
+
+module.exports.deleteaccount = function(req,res){
+    return res.render('Profile/deleteAccount',{title:'Delete Account',csrfToken:req.csrfToken()})
+}
+
+module.exports.deleteaccountform = function(req,res){
+    User.findOne({email:req.body.email},async function(err,user){
+        if(err){
+            console.log('Error in finding user',err)
+            return
+        }
+        if((await bcrypt.compare(req.body.password,user.password))==false){
+            req.flash('error','Incorrect Password is entered')
+            return res.redirect('/profile/deleteaccount')
+        }
+        await User.findOneAndDelete({email:req.body.email})
+        req.flash('info','Your account is deactivated')
+        return res.redirect('/users/signin')
     })
 }
--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const csrf = require('csurf')
+const csrfProtection = csrf()
 
 const profileController = require('../controllers/profileController')
 
-router.get('/view/:id',passport.checkAuth,profileController.view)
-router.get('/edit/:id',passport.checkAuth,profileController.edit)
+router.get('/view',passport.checkAuth,profileController.view)
+router.get('/edit',passport.checkAuth,profileController.edit)
 router.post('/editprofile',passport.checkAuth,(profileController.upload).single('profileimg'),profileController.editprofile)
+router.get('/settings',passport.checkAuth,profileController.settings)
+router.get('/deleteaccount',passport.checkAuth,csrfProtection,profileController.deleteaccount)
+router.post('/deleteaccountform',passport.checkAuth,profileController.deleteaccountform)
 
 module.exports = router
